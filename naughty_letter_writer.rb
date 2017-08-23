be19dd5ec3467abbac38a9e2dcd256e9ec2aecd1
Navi Mann <navdeep.k.mann@gmail.com>
2017-08-23 15:17:51 -0400

    completed naughty letters

---
+++ naughty_letter_writer.rb
@@ -0,0 +1,29 @@
+require 'erb'
+
+kids_data   = File.read('data/kids-data.txt')
+naughty_letter = File.read('templates/naughty_letter_template.txt.erb')
+
+kids_data.each_line do |kid|
+
+  kid_data_array = kid.split
+
+  name     = kid_data_array[0]
+  gender   = kid_data_array[1]
+  behavior = kid_data_array[2]
+  toys     = kid_data_array[3..8]
+  infraction = kid.split("|")[1]
+
+  next unless behavior == 'naughty'
+  toys1 = kid_data_array[3..8]
+  toys1.delete('Kaleidoscope')
+  toy = toys1.sample
+
+  filename    = 'letters/naughty/' + name + '.txt'
+  letter_text = ERB.new(naughty_letter, nil, '-').result(binding)
+
+  puts "Writing #{filename}."
+  File.write(filename, letter_text)
+
+end
+
+puts 'Done!'
